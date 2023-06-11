218c48d1ed51f4b9e3cf7c952a61c4e06b12bb01
ucat <ucat.games@gmail.com>
2023-06-11 12:07:12 +0800

    lib

--- crates/dbproxy/src/lib.rs
+++ crates/dbproxy/src/lib.rs
@@ -1,16 +1,33 @@
-use std::any::{Any};
+use std::any::Any;
 
-use thrift::protocol::{TCompactInputProtocolFactory, TCompactOutputProtocolFactory};
-use thrift::transport::{TFramedReadTransportFactory, TFramedWriteTransportFactory};
-use thrift::server::TServer;
+use thrift::protocol::{TCompactInputProtocolFactory, TCompactOutputProtocolFactory, TCompactInputProtocol, TCompactOutputProtocol, TInputProtocol, TOutputProtocol};
+use thrift::transport::{TFramedReadTransportFactory, TFramedWriteTransportFactory, TFramedReadTransport, TFramedWriteTransport, ReadHalf, WriteHalf, TTcpChannel };
+use thrift::server::{TServer, TProcessor};
 
-use proto::dbproxy::DbproxySyncHandler;
+use proto::dbproxy::{DbproxySyncHandler, DbproxySyncProcessor};
 use proto::hub::HubDbproxyCallbackSyncClient;
 
 use mongo::MongoProxy;
 use processor::Processor;
 
 mod db;
+
+type HubInputProtocol = TCompactInputProtocol<TFramedReadTransport<ReadHalf<TTcpChannel>>>;
+type HubOutputProtocol = TCompactOutputProtocol<TFramedWriteTransport<WriteHalf<TTcpChannel>>>;
+
+struct DbproxySyncExtendProcessor<H: DbproxySyncHandler> {
+    processor: DbproxySyncProcessor<H>,
+    client: Box<HubDbproxyCallbackSyncClient<HubInputProtocol, HubOutputProtocol>>
+}
+
+impl <H: DbproxySyncHandler> TProcessor for DbproxySyncExtendProcessor<H> {
+    fn process(&self, i_prot: &mut dyn TInputProtocol, o_prot: &mut dyn TOutputProtocol) -> thrift::Result<()> {
+        let io = HubInputProtocol::new(i_prot);
+        self.client = Box::new(HubDbproxyCallbackSyncClient::new(i_prot as HubInputProtocol, o_prot));
+
+        self.processor.process(i_prot, o_prot)
+    }
+}
 
 pub struct DBProxyThriftServer {
     proxy : MongoProxy,
@@ -25,7 +42,7 @@ impl DBProxyThriftServer {
         }
     }
 
-    fn cost_mut(&self) -> &mut Self {
+    fn cast_mut(&self) -> &mut Self {
         unsafe { &mut * (self as * const Self as * mut Self) }
     }
 }
@@ -37,7 +54,7 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_get_guid(&self, db: String, collection: String, callback_id: String) -> thrift::Result<()> {
         let ev_data = db::DBEvGetGuid::new();
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
@@ -45,7 +62,7 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_create_object(&self, db: String, collection: String, callback_id: String, object_info: Vec<u8>) -> thrift::Result<()> {
         let ev_data = db::DBEvCreateObject::new(object_info);
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
@@ -53,7 +70,7 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_updata_object(&self, db: String, collection: String, callback_id: String, query_info: Vec<u8>, updata_info: Vec<u8>, _upsert: bool) -> thrift::Result<()> {
         let ev_data = db::DBEvUpdataObject::new(query_info, updata_info, _upsert);
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
@@ -61,15 +78,15 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_find_and_modify(&self, db: String, collection: String, callback_id: String, query_info: Vec<u8>, updata_info: Vec<u8>, _new: bool, _upsert: bool) -> thrift::Result<()> {
         let ev_data = db::DBEvFindAndModify::new(query_info, updata_info, _new, _upsert);
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
     }
 
     fn handle_remove_object(&self, db: String, collection: String, callback_id: String, query_info: Vec<u8>) -> thrift::Result<()> {
-        let mut ev_data = db::DBEvRemoveObject::new(query_info);
-        let p = self.cost_mut();
+        let ev_data = db::DBEvRemoveObject::new(query_info);
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
@@ -77,7 +94,7 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_get_object_info(&self, db: String, collection: String, callback_id: String, query_info: Vec<u8>, skip: i32, limit: i32, sort: String, ascending: bool) -> thrift::Result<()> {
         let ev_data = db::DBEvGetObjectInfo::new(query_info, skip, limit, sort, ascending);
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
@@ -85,7 +102,7 @@ impl DbproxySyncHandler for DBProxyThriftServer {
 
     fn handle_get_object_count(&self, db: String, collection: String, callback_id: String, query_info: Vec<u8>) -> thrift::Result<()> {
         let ev_data = db::DBEvGetObjectCount::new(query_info);
-        let p = self.cost_mut();
+        let p = self.cast_mut();
         let ev = db::DBEvent::new(&mut p.proxy, db::DBEventType::EvGetGuid, db, collection, callback_id, Box::new(ev_data));
         p.processor.enque(Box::new(ev));
         Ok(())
