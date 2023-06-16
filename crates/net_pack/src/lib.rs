7aabc552afedecf2e0823aa6cb18eabe0b73e615
qianqians <qianqians.games@gmail.com>
2023-06-16 10:38:04 +0800

    net pack

--- crates/net_pack/src/lib.rs
+++ crates/net_pack/src/lib.rs
@@ -1,13 +1,11 @@
 pub struct NetPack {
-    buf: Vec<u8>,
-    idx: u32
+    buf: Vec<u8>
 }
 
 impl NetPack {
     pub fn new() -> NetPack {
         NetPack {
-            buf: Vec::new(),
-            idx: 0
+            buf: Vec::new()
         }
     }
 
@@ -16,17 +14,17 @@ impl NetPack {
     }
 
     pub fn try_get_pack(&mut self) -> Option<Vec<u8>> {
-        let len0 = self.buf[0] as u32;
-        let len1 = self.buf[1] as u32;
-        let len2 = self.buf[2] as u32;
-        let len3 = self.buf[3] as u32;
-        let new_pack_len = len0 | len1 << 8 | len2 << 16 | len3 << 24;
-        if new_pack_len > self.idx {
+        let len0 = self.buf[0] as usize;
+        let len1 = self.buf[1] as usize;
+        let len2 = self.buf[2] as usize;
+        let len3 = self.buf[3] as usize;
+        let new_pack_len: usize = len0 | len1 << 8 | len2 << 16 | len3 << 24;
+        if new_pack_len > self.buf.len() {
             None
         }
         else {
             let len = new_pack_len as usize;
-            let idx = self.idx as usize;
+            let idx = self.buf.len();
             let mut buf = vec![0u8; len];
             buf.copy_from_slice(&self.buf[..len]);
 
@@ -36,11 +34,9 @@ impl NetPack {
                 tmp.copy_from_slice(&self.buf[len..idx]);
                 self.buf.clear();
                 self.buf.copy_from_slice(&tmp[..]);
-                self.idx = remain as u32;
             }
             else {
                 self.buf.clear();
-                self.idx = 0;
             }
 
             Some(buf)
