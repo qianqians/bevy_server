640dd84d3dc6583aa1913b213c5265546c08744a
qianqians <qianqians.games@gmail.com>
2023-06-23 16:04:24 +0800

    dbproxy

--- crates/log/src/lib.rs
+++ crates/log/src/lib.rs
@@ -1,5 +1,14 @@
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_appender::{non_blocking, rolling};
+use tracing_subscriber::{filter::EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
-pub fn init() {
-    tracing_subscriber::registry().with(fmt::layer()).init()
+pub fn init(filter: String, log_dir: String, log_file: String) {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(filter));
+    let file_appender = rolling::never(log_dir, log_file);
+    let (non_blocking_appender, _guard) = non_blocking(file_appender);
+    let file_layer = fmt::layer().with_ansi(false).with_writer(non_blocking_appender);
+
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(file_layer)
+        .init()
 }
--- src/dbproxy_main.rs
+++ src/dbproxy_main.rs
@@ -4,8 +4,8 @@ use serde::{Deserialize, Serialize};
 
 use tracing::{trace, debug, info, warn, error};
 
-use proto::hub;
 use config::{load_data_from_file, load_cfg_from_data};
+use log;
 use dbproxy::DBProxyServer;
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -17,16 +17,36 @@ struct DBProxyCfg {
     log_dir: String
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg_file = &args[0];
 
     let cfg_data = match load_data_from_file(cfg_file.to_string()) {
         Err(e) => {
-            error!("DBProxy load_data_from_file faild {}", cfg_file);
+            error!("DBProxy load_data_from_file faild {}, {}!", cfg_file, e);
             return;
         },
         Ok(_cfg_data) => _cfg_data
     };
-    let cfg = load_cfg_from_data::<DBProxyCfg>(&cfg_data);
+    let cfg = match load_cfg_from_data::<DBProxyCfg>(&cfg_data) {
+        Err(e) => {
+            error!("DBProxy load_cfg_from_data faild {}, {}!", cfg_data, e);
+            return;
+        },
+        Ok(_cfg) => _cfg
+    };
+    log::init(cfg.log_level, cfg.log_dir, cfg.log_file);
+
+    let host = format!("0.0.0.0:{}", cfg.port);
+    let mut server = match DBProxyServer::new(cfg.mongo_url, host).await {
+        Err(e) => {
+            error!("DBProxy DBProxyServer new faild {}!", e);
+            return;
+        },
+        Ok(_s) => _s
+    };
+
+    server.run().await;
+    server.join().await;
 }
